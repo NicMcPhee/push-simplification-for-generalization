b4f53c87fa1e0b7c0d4142bc9a1eac3a20668287
Nic McPhee <mcphee@morris.umn.edu>
2017-01-12 09:35:24 -0600

    Fix error in no-op regex
    Genomes have “no-op” where programs have “noop”, and my regex only handled the latter case. I modified that to include an optional dash in the middle, and added the parsing of some additional lines.

--- scripts/simplification-analysis/src/simplification_analysis/count_noops.clj
+++ scripts/simplification-analysis/src/simplification_analysis/count_noops.clj
@@ -18,15 +18,23 @@
 
 (defn count-noops
   [str]
-  (count (re-seq #"noop" str)))
+  (count (re-seq #"no(-)?op" str)))
 
 (defn count-noops-for-file
   [data-file]
   (let [lines (s/split-lines (slurp data-file))
-        genome-lines (filter #(s/starts-with? % "Genome:") lines)
-        simplified-lines (filter #(s/starts-with? % "Simplified Genome:") lines)]
-      {:initial-noop-count (count-noops (first genome-lines))
-       :simplified-noop-counts (frequencies (map count-noops simplified-lines))}))
+        genome-line (first (filter #(s/starts-with? % "Genome:") lines))
+        program-line (first (filter #(s/starts-with? % "Program:") lines))
+        total-train-error-line (first (filter #(s/starts-with? % "Total Train Error:") lines))
+        total-test-error-line (first (filter #(s/starts-with? % "Total Test Error:") lines))
+        simplified-genome-lines (filter #(s/starts-with? % "Simplified Genome:") lines)
+        simplified-program-lines (filter #(s/starts-with? % "Simplified Program:") lines)]
+      {:initial-genome-noop-count (count-noops genome-line)
+       :initial-program-noop-count (count-noops program-line)
+       :total-train-error (read-string (nth (s/split total-train-error-line #" ") 3))
+       :total-test-error (read-string (nth (s/split total-test-error-line #" ") 3))
+       :simplified-genome-noop-counts (frequencies (map count-noops simplified-genome-lines))
+       :simplified-program-noop-counts (frequencies (map count-noops simplified-program-lines))}))
 
 (defn count-noops-for-files
   [data-files]
